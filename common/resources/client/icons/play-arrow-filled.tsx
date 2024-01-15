69e4874ed1e97157600e51bbd784e6dcd212e034
Peter Nagy <nagy.pite@gmail.com>
2024-01-15 11:19:12 +0100

    add RTL reverse version for small play icon

--- common/resources/client/icons/play-arrow-filled.tsx
+++ common/resources/client/icons/play-arrow-filled.tsx
@@ -2,5 +2,6 @@ import {createSvgIcon} from '@common/icons/create-svg-icon';
 
 export const PlayArrowFilledIcon = createSvgIcon(
   <path d="M8 19V5l11 7Z" />,
-  'PlayArrowFilled'
+  'PlayArrowFilled',
+  undefined, true
 );
