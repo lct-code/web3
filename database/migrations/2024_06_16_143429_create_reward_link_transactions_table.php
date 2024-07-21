ef70cfd12f19ef230c2c2ac086fa9e934dc9c104
lct-code <lifeconnectionTechnology@gmail.com>
2024-07-21 10:21:34 +0400

    Remove foreign fkey

--- database/migrations/2024_06_16_143429_create_reward_link_transactions_table.php
+++ database/migrations/2024_06_16_143429_create_reward_link_transactions_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
 
             $table->foreign('reward_link_id')->references('id')->on('reward_links')->onDelete('cascade');
-            $table->foreign('user_id');
         });
     }
 
