e05de1f1c17874936ab3689829b77b4a38ceb34c
MA <edu.mahmoud.ahmed@gmail.com>
2025-01-01 16:30:25 +0330

    debug: better error logs

--- common/Billing/Gateways/ZainSd/ZainSd.php
+++ common/Billing/Gateways/ZainSd/ZainSd.php
@@ -193,7 +193,10 @@ class ZainSd implements CommonSubscriptionGatewayActions
         //     $data['success'] = true;
         //     $data['error_code'] = 0;
         // }
-        $this->handleZainSdError($data, 'syncSubscriptionDetails');
+        $dataError = $data;
+        $dataError['phone'] = $phone;
+        $dataError['product_code'] = $productCode;
+        $this->handleZainSdError($dataError, 'syncSubscriptionDetails');
 
         // Find subscription by phone and product code
         $subscription = Subscription::where('gateway_id',$data['subscription_data']['id'])->first();
