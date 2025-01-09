bde64a75aa8ab983b44151f6250b4c1445f6cb09
MA <edu.mahmoud.ahmed@gmail.com>
2025-01-09 13:29:36 +0330

    REF: skip authentication for zain_sd payment method

--- common/resources/client/auth/guards/not-subscribed-route.tsx
+++ common/resources/client/auth/guards/not-subscribed-route.tsx
@@ -9,7 +9,7 @@ export function NotSubscribedRoute({children}: GuestRouteProps) {
   const {isLoggedIn, isSubscribed} = useAuth();
   const {paymentMethodId} = useParams();
   const {pathname} = useLocation();
-  const noLogin = paymentMethodId === 'zain_sd' || paymentMethodId === 'phonesub' || pathname.split('/').includes('phonesub') ;
+  const noLogin = paymentMethodId === 'zain_sd' || paymentMethodId === 'phonesub' || pathname.split('/').includes('phonesub') || pathname.split('/').includes('zain_sd') ;
   
   if (!isLoggedIn && !noLogin) {
     return <Navigate to="/register" replace />;
